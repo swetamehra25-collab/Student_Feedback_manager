550a090aadef4c4a73f744242396546e40484c6d
Sweta Mehra <swetamehra25@navgurukul.org>
2026-09-19 17:55:23 +0530

    updated file name

--- frontend/feedback.js
+++ frontend/feedback.js
@@ -8,7 +8,7 @@ form.addEventListener("submit", async (event) => {
     const rating = document.getElementById("rating").value;
     const comment = document.getElementById("comment").value;
 
-    const response = await fetch("https://student-feedback-manager-1-b8um.onrender.com/feedback", {
+    const response = await fetch("https://student-feedback-manager-3-om5c.onrender.com/feedback", {
         method: "POST",
 
         headers: {
